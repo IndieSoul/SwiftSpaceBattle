7eaa350135a12ea586e18454456bd8015650add2
Luis Enrique Rosas Espinoza <enriquerosas@lesscoded.dev>
2025-01-12 00:42:32 -0600

    Initial Commit

---
+++ SwiftSpaceBattle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSpaceBattle
+//
+//  Created by Luis Enrique Rosas Espinoza on 12/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSpaceBattle/SwiftSpaceBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSpaceBattleApp.swift
+//  SwiftSpaceBattle
+//
+//  Created by Luis Enrique Rosas Espinoza on 12/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSpaceBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
